9ff5705dd50cc8be98d0f44c364ab185bb8b09e1
Larry Miles <larry@blackmondaydesign.com>
2026-04-16 10:07:20 +0100

    Committing  more pre-work code

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,13 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+// Select which type of licence applies to this property
+router.post('/do-you-use-a-letting-agent-to-manage-this-property', function(request, response) {
+
+    var usingLettingAgentToManage = request.session.data['usingLettingAgentToManage']
+    if (usingLettingAgentToManage == "Yes"){
+        response.redirect("#")
+    } else {
+        response.redirect("/letting_agents/select_which_type_of_licence_applies_to_this_property")
+    }
+})
